8ea3532dbd908cfefcaa94878f526e208abc0066
Sergey Spirin <sergey@MacBook-Pro-sergey.local>
2021-11-07 12:19:14 +0300

    Added header file support

--- Lab-1/main.cpp
+++ Lab-1/main.cpp
@@ -2,69 +2,9 @@
 #include <string>
 #include <fstream>
 #include <iomanip>
+#include "explorer.h"
 
 using namespace std;
-
-class File
-{    
-    string catalog_name;    
-    string file_name;
-    float file_size;
-
-public:
-    File();
-    ~File(); 
-    File(string new_catalog_name, string new_file_name, float new_file_size);    
-
-    void setCatalogName(string new_catalog_name);
-    void setFileName(string new_file_name);
-    void setFileSize(float new_file_size);
-
-    string getCatalogName();
-    string getFileName();
-    float getFileSize();
-};
-
-File::File() {};
-
-File::~File() {};
-
-File::File(string new_catalog_name, string new_file_name, float new_file_size)
-{
-    catalog_name = new_catalog_name;
-    file_name = new_file_name;
-    file_size = new_file_size;
-};
-
-void File::setCatalogName(string new_catalog_name)
-{
-    catalog_name = new_catalog_name;
-};
-
-void File::setFileName(string new_file_name)
-{
-    file_name = new_file_name;
-};
-
-void File::setFileSize(float new_file_size)
-{
-    file_size = new_file_size;
-};
-
-string File::getCatalogName()
-{
-    return catalog_name;
-};
-
-string File::getFileName()
-{
-    return file_name;
-};
-
-float File::getFileSize()
-{
-    return file_size;
-};
 
 int getMaxCatalogLength(File* files, int rows)
 {
